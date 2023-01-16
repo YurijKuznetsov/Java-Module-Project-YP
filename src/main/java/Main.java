1c2b779f5622029f4516472ce69613aeae9bf324
YurijKuznetsov <122029440+YurijKuznetsov@users.noreply.github.com>
2023-01-16 23:30:15 +0300

    Revert "Курсовая работа №1"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,8 @@
-
-
-// dev branch for Y.Practicum
 public class Main {
-    public static void main(final String[] args) {
-        final Calculator calculator = new Calculator();
-        calculator.numberOfPerson();
-        String rub = "";
-        calculator.countProduct();
-        double sumResult = calculator.sum / calculator.number;
-        if ((Math.floor(sumResult) % 100) >= 11 && (Math.floor(sumResult) % 100) <= 14) {
-            rub = "рублей";
-        } else {
-           if ((Math.floor(sumResult) % 10) == 1) {
-            rub = "рубль";
-           } else if ((Math.floor(sumResult) % 10) > 1 && (Math.floor(sumResult) % 10) < 5) {
-            rub = "рубля";
-           } else if ((Math.floor(sumResult) % 10) > 5) {
-               rub = "рублей";
-           }
-        }
-        String line = String.format("Добавленные товары:\n%s\nцена, которую должен заплатить каждый %.2f %s", calculator.result, sumResult, rub);
-        System.out.println(line);
+
+    public static void main(String[] args) {
+        // ваш код начнется здесь
+        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
+        System.out.println("Привет Мир");
     }
 }
-
